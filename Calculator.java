38ce8f97202826c561d06daead333cc7264c5291
ccwinking <154419434+ccwinking@users.noreply.github.com>
2023-12-20 17:47:22 +0800

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,8 +1,7 @@
-class BasicCalculator {
+class Calculator {
     private int total = 0;
-    public void calc(char curOperator, int num) {
-        int before = total;
-        switch (curOperator) {
+    public void doCalculate(char oper, int num) {
+        switch (oper) {
             case '+':
                 total += num;
                 break;
@@ -16,34 +15,33 @@ class BasicCalculator {
                 total /= num;
                 break;
         }
-        System.out.println(before +"" + curOperator +num+"=" + total);
     }
 }
 
 
 abstract class Command {
-       abstract public void execute();
-       abstract public void reverse();
-   }
+   abstract public void execute();
+   abstract public void reverse();
+}
 
 
 class CalculatorCommand extends Command {
-    char curOperator;
+    char oper;
     int num;
-    BasicCalculator basicCalculator;
+    Calculator calculator;
  
-    public CalculatorCommand(BasicCalculator basicCalculator, char curOperator, int num) {
-        this.basicCalculator = basicCalculator;
-        this.curOperator = curOperator;
+    public CalculatorCommand(Calculator calculator, char oper, int num) {
+        this.Calculator = calculator;
+        this.oper = oper;
         this.num = num;
     }
  
-    public char getCurOperator() {
-        return curOperator;
+    public char getOper() {
+        return oper;
     }
  
-    public void setCurOperator(char curOperator) {
-        this.curOperator = curOperator;
+    public void setOper(char oper) {
+        this.oper = oper;
     }
  
     public int getNum() {
@@ -54,22 +52,22 @@ class CalculatorCommand extends Command {
         this.num = num;
     }
  
-    public BasicCalculator getCalculator() {
-        return basicCalculator;
+    public Calculator getCalculator() {
+        return Calculator;
     }
  
-    public void setCalculator(BasicCalculator basicCalculator) {
-        this.basicCalculator = basicCalculator;
+    public void setCalculator(Calculator Calculator) {
+        this.Calculator = Calculator;
     }
  
     @Override
     public void execute() {
-        basicCalculator.calc(curOperator, num);
+        Calculator.doCalculate(oper, num);
     }
  
     @Override
     public void reverse() {
-        basicCalculator.calc(undo(curOperator), num);
+        Calculator.doCalculate(undo(oper), num);
     }
  
     private char undo(char op) {
@@ -93,57 +91,38 @@ class CalculatorCommand extends Command {
 }
 
 
-public class UndoRedoCalculator {
-        private BasicCalculator basicCalculator = new BasicCalculator();
-        private List<Command> commands = new ArrayList<Command>();
-        private int currentIndex = 0;
- 
-        //计算
-        public void calc(char curOperator, int num) {
-            Command command = new CalculatorCommand(basicCalculator, curOperator, num);
-            command.execute();
-            commands.add(command);
-            currentIndex++;
-        }
- 
-        //重做
-        public void redo(int levels) {
-            System.out.println("Redo" + levels);
-            for (int i = 0; i < levels; i++)
-                if (currentIndex < commands.size() - 1) {
-                    ((Command) commands.get(currentIndex++)).execute();
-                }else {
-                    System.out.println("没有可redo的数据!");
-                }
-        }
- 
-        //撤销
-        public void undo(int levels) {
-            System.out.println("Undo" + levels);
-            for (int i = 0; i < levels; i++)
-                if (currentIndex > 0) {
-                    int index = --currentIndex;
-                    Command command= (Command)commands.get(index);
-                    command.reverse();
-                }else {
-                    System.out.println("没有可undo的数据!");
-                }
+public class EnhanceCalculator {
+    private Calculator calculator = new Calculator();
+    private List<Command> commands = new ArrayList<Command>();
+    private int curIndex = 0;
+
+    //计算doCal
+    public void doCalculate(char oper, int num) {
+        Command command = new CalculatorCommand(calculator, oper, num);
+        command.execute();
+        commands.add(command);
+        curIndex++;
+    }
+
+    //重做redo
+    public void doRedoCal(int levels) {
+        for (int i = 0; i < levels; i++) {
+            if (curIndex < commands.size() - 1) {
+                ((Command) commands.get(curIndex++)).execute();
             }
         }
+    }
 
+    //撤销undo
+    public void doUndoCal(int levels) {
+        for (int i = 0; i < levels; i++) {
+            if (curIndex > 0) {
+                int index = --curIndex;
+                Command command= (Command)commands.get(index);
+                command.reverse();
+            }
+        }
+    }
 
-class RunMain {
-     public static UndoRedoCalculator calculator = new UndoRedoCalculator();
- 
-     public static void main(String[] args) {
-         calculator.calc('+', 100);//加法
-         calculator.calc('-', 50);//减法
-         calculator.calc('*', 10);//乘法
-         calculator.calc('/', 2);//除法
-         calculator.undo(4);//撤销前四个步骤
-         calculator.redo(3);//再次执行三个步骤
-         //再次执行
-         calculator.calc('+', 2);//加法
-     }
- }
+}
 
